2cdfc2ecdd7202284a3b15615c692d214f304fc3
Kaktusz Sok <eeveelutionksp@gmail.com>
2022-10-15 15:18:50 +0200

    Changed subcommands to full name

--- src/main/java/kaktusz/geopolitika/commands/CmdState.java
+++ src/main/java/kaktusz/geopolitika/commands/CmdState.java
@@ -7,8 +7,8 @@ import kaktusz.geopolitika.commands.subcommands.state.CmdStateDeposit;
 public class CmdState extends CommandRoot {
 	public CmdState() {
 		super("state", CommandPermissions.ANYONE);
-		addSubcommand(new CmdStateBalance("bal", CommandPermissions.ANYONE));
-		addSubcommand(new CmdStateDeposit("dep", CommandPermissions.ANYONE));
+		addSubcommand(new CmdStateBalance("balance", CommandPermissions.ANYONE));
+		addSubcommand(new CmdStateDeposit("deposit", CommandPermissions.ANYONE));
 		addSubcommand(new CmdStateCooldown("cooldown", CommandPermissions.OP));
 	}
 }
